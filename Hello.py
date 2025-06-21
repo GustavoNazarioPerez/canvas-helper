7990cb28dccee1af481a3d1c1b618bfaec80b5ac
Dript1220 <theonetostark12@gmail.com>
2025-06-21 14:51:30 -0400

    gustavo test3

--- Hello.py
+++ Hello.py
@@ -1,1 +1,9 @@
-print("hello")
+from dotenmv import load_dotenv
+load_dotenv()
+import os
+import os
+
+CANVAS_TOKEN = os.getenv("CANVAS_TOKEN")
+CANVAS_DOMAIN = os.getenv("CANVAS_DOMAIN")
+PUSHOVER_USER = os.getenv("PUSHOVER_USER")
+PUSHOVER_TOKEN = os.getenv("PUSHOVER_TOKEN")
