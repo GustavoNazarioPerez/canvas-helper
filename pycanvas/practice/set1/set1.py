0b35473cd6c8cfbbcfbd0c39ee509554f0de1703
Dript1220 <theonetostark12@gmail.com>
2025-06-25 22:00:58 -0400

    changes to pycanvas

--- pycanvas/practice/set1/set1.py
+++ pycanvas/practice/set1/set1.py
@@ -5,13 +5,15 @@ def is_even(n: int) -> bool | None:
 
 
 def average(numbers: list[float]) -> float | None:
-    """Return the average of a list of floats. Raises ValueError if the list is empty."""
+    """
+    Return the average of a list of floats.
+    Raises ValueError if the list is empty.
+    """
     # Add your code here
     pass
 
 
 def is_positive(n: int) -> bool | None:
-    """Reutrn True if the number is positive, False otherwise"""
+    """Return True if the number is positive, False otherwise"""
     # Add your code here
     pass
-
